17381b2cd8b6785b1bb7ccb96e8676f2f60ef9d0
luan-services <luandotservices@gmail.com>
2026-03-24 21:35:47 -0300

    feat: updated index.ts to add initial data fetch test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from 'hono'
+import { fetchGithubUserData, Bindings } from './lib/fetch-userdata'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Cloudflare Workers!')
+app.get('/', (c) => c.text('Hello Cloudflare Workers!'));
+
+app.get('/api', async (c) => {
+	const username = c.req.query('username')
+
+	if (!username) { /* must implement an error handler to send json error responses */
+		throw new Error('No username query provided');
+	}
+
+	try {
+		const data = await fetchGithubUserData(username, c.env);
+		return c.json(data); 
+	} catch (error) { /* must implement an error handler to send json error responses */
+		throw new Error('Unknown Error');
+	}
 })
 
 export default app
